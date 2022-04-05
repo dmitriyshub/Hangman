7628e6b2261558d4778c8ee3fc3ce9e9038bf0c7
dmitriyshub <d.shtranvasser@gmail.com>
2022-04-06 00:06:18 +0300

    Hangman welcome messege

---
+++ Hangman-unit1.py
@@ -0,0 +1,17 @@
+import random
+print("Welcome to the game Hangman")
+print(""" 
+  _    _                                         
+ | |  | |                                        
+ | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
+ |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+ | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+ |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                      __/ |                      
+                     |___/
+ 
+ 
+ """)
+
+randomNumber = random.randint(5, 10)
+print(randomNumber)
