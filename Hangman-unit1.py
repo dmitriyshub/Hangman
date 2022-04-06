f9500a0c1389c909fee161f25d03ed741070ee9c
dmitriyshub <d.shtranvasser@gmail.com>
2022-04-06 10:56:14 +0300

    Adding the hangman pictures

--- Hangman-unit1.py
+++ Hangman-unit1.py
@@ -1,4 +1,5 @@
 import random
+#the Hangman welcome page
 print("Welcome to the game Hangman")
 print(""" 
   _    _                                         
@@ -12,6 +13,61 @@ print("""
  
  
  """)
-
+# number of attempts (randomly)
 randomNumber = random.randint(5, 10)
 print(randomNumber)
+
+
+#the Hangman pictures
+
+print("picture 1:")
+print("x-------x")
+
+print("picture 2:")
+print("""x-------x
+    |
+    |
+    |
+    |
+    |""")
+
+print("picture 3:")
+print("""x-------x
+    |       |
+    |       0
+    |
+    |
+    |""")
+
+print("picture 4:")
+print("""x-------x
+    |       |
+    |       0
+    |       |
+    |
+    |""")
+
+print("picture 5:")
+print("""x-------x
+    |       |
+    |       0
+    |      /|\\
+    |
+    |""")
+
+print("picture 6:")
+print("""x-------x
+    |       |
+    |       0
+    |      /|\\
+    |      /
+    |""")
+
+print("picture 7:")
+print("""x-------x
+    |       |
+    |       0
+    |      /|\\
+    |      / \\
+    |""")
+
