50957e3bbc95a7b17ec66047d8c6faf28790009a
dmitriyshub <d.shtranvasser@gmail.com>
2022-04-12 11:43:29 +0300

    adding conditions to input answer

--- Hangman-run.py
+++ Hangman-run.py
@@ -23,6 +23,15 @@ letter_answer = input("  Guess a letter please: ")
 letter_answer = letter_answer.lower()
 print("  Your answer is: ", letter_answer)
 
+if letter_answer.isalpha() == False and len(letter_answer) > 1:
+    print("E3")
+elif letter_answer.isalpha() == False:
+    print("E2")
+elif len(letter_answer) > 1:
+    print("E1")
+else:
+    print("your doing something wrong")
+
 #building a game table
 word_answer = input("Please enter a word:  ")
 
