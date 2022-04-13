d4c40aca1bf77a301f528128d9155834a291fa61
dmitriyshub <d.shtranvasser@gmail.com>
2022-04-13 10:05:58 +0300

    little changes of strings file

--- strings.py
+++ strings.py
@@ -1,17 +1,19 @@
-#answer = input("Please enter a string:")
-#first_letter = answer[0]
-#rest_sentence = answer[1:].replace("d", "e")
-#final_answer = first_letter + rest_sentence
+# replace letters in the string:
 
-#print(final_answer)
-
-"""the answer:
+""" first:
+answer = input("Please enter a string:")
+first_letter = answer[0]
+rest_sentence = answer[1:].replace("d", "e")
+final_answer = first_letter + rest_sentence
+print(final_answer)
+"""
+""" second: (final answer)
 fix_me = input("Enter a string: ")
 first_letter = fix_me[0]
 print(first_letter + fix_me[1:].replace(first_letter, 'e'))
 """
 
-
+# input a string and get an output with the string that contains first lower case chars and the other half with upper case
 user_string = input("Please enter your string: ")
 print(int(len(user_string)) // 2)
 first = user_string[0:4]
