50272632779a6311208cd1dcf320168a2a16bf0e
dmitriyshub <d.shtranvasser@gmail.com>
2022-04-14 09:43:56 +0300

    adding filter_teens function

---
+++ filter_teens.py
@@ -0,0 +1,35 @@
+def filter_teens(a = 13, b = 13, c = 13):
+
+    return fix_age(a) + fix_age(b) + fix_age(c)
+
+
+def fix_age(age):
+    if (age >=13) and (age <= 19) and (age != 15) and (age != 16):
+        age = 0
+        return age
+    else:
+        return age
+
+
+"""sum = 0
+def filter_teens(a = 13,b = 13,c = 13):
+    age_list = [a,b,c]
+    new_age_list = []
+    for i in age_list:
+
+    if(a or b or c >=13 <= 19 and (a or b or c != 15 or 16)) :
+        for i in age_list:
+            new_age_list.append(fix_age(i))
+        else:
+            new_age_list.append(i)
+        return new_age_list
+
+
+print(filter_teens(2,13,1))
+
+
+
+Final_Result = filter_teens()
+print(Final_Result)
+"""
+
