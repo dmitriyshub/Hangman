bc1a2379571711225349a407dda7e35754c15d8a
dmitriyshub <d.shtranvasser@gmail.com>
2022-04-14 09:43:28 +0300

    fixing distance function

--- distance.py
+++ distance.py
@@ -13,9 +13,7 @@ b = abs(num2)
 c = abs(num3)
 closed_absolute = 1
 print(a,b,c)
-
 if [(abs(num2-num1)) == 1 or (abs(num3-num1) == 1)] and [(abs(num2 - num1) > 1) and (abs(num3 - num2) > 1) or (abs(num3 - num1) > 1) and(abs(num3 - num2) > 1)]:
-
     return True
 else:
     return False"""
