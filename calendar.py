4dbb15f7c954e9e18b090dc91af321d639a2bb65
dmitriyshub <d.shtranvasser@gmail.com>
2022-04-15 12:05:48 +0300

    adding explanation comments for calendar.py file

--- calendar.py
+++ calendar.py
@@ -1,15 +1,18 @@
 import calendar
-#week_days = ["Sunday", "Monday", "Tuesday", "Wendnesday", "Thursday", "Friday", "Saturday"]
-calendar.setfirstweekday(6)
-date = input("Please specify a date: ('dd/mm/yyyy')")
-dd = int(date[:2])
-mm = int(date[3:5])
-yyyy = int(date[6:])
+
+#week_days = ["Sunday", "Monday", "Tuesday", "Wendnesday", "Thursday", "Friday", "Saturday"] (Can make an implamantation of this function with list later)
+
+calendar.setfirstweekday(6) # the default first number in the calendar function is Monday[0] and the last is Sunday[6]
+date = input("Please specify a date: ('dd/mm/yyyy')") # input from user with Date
+
+dd = int(date[:2]) # slicing of Days
+mm = int(date[3:5]) # slicing of Months
+yyyy = int(date[6:]) # slicing od Years
 
 
-cal_weekday = calendar.weekday(yyyy, mm, dd)
+cal_weekday = calendar.weekday(yyyy, mm, dd) # weekday method of cal function is check for day number of the week
 
-
+# Conditions for day weeks
 if cal_weekday == 6:
     print('Sunday')
 elif cal_weekday == 0:
