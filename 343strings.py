dc296591b23a21a4e7148dd4e333ef0d21186d5f
dmitriyshub <d.shtranvasser@gmail.com>
2022-04-16 00:07:16 +0300

    changing file name string to343 string and change some logic

--- 343strings.py
+++ 343strings.py
@@ -15,9 +15,9 @@ print(first_letter + fix_me[1:].replace(first_letter, 'e'))
 
 # input a string and get an output with the string that contains first lower case chars and the other half with upper case
 user_string = input("Please enter your string: ")
-print(int(len(user_string)) // 2)
-first = user_string[0:4]
-second = user_string[4:]
+len_string = int(len(user_string)) // 2
+first = user_string[0:len_string]
+second = user_string[len_string:]
 first = first.lower()
 second = second.upper()
 print(first + second)
