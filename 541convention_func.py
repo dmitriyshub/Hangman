86283f07356dc3863a5c2425e1e9366a2925fdfb
dmitriyshub <d.shtranvasser@gmail.com>
2022-04-15 23:53:04 +0300

    adding convention func

---
+++ 541convention_func.py
@@ -0,0 +1,24 @@
+def func(num1, num2):
+    """
+    the function func() get two numbers and returns the sum value
+    :param num1: number value
+    :param num2: number value
+    :type num1: integer
+    :type num2: integer
+    :return: the sum of both numbers
+    :rtype: integer
+    """
+    return num1 + num2
+
+
+
+
+
+
+
+def main():
+    print(func(1, 1))
+    help(func(1, 1))
+
+if __name__ == "__main__":
+    main()
