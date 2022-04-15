6c6148df510c3f154db5a13f608497785311782b
dmitriyshub <d.shtranvasser@gmail.com>
2022-04-15 12:16:35 +0300

    adding explanation comments for chocolate_maker.py file

--- chocolate_maker.py
+++ chocolate_maker.py
@@ -1,4 +1,11 @@
 def chocolate_maker(small, big, x):
+    """
+    This function calculates if the all chocloate pieces can be placed in X (chocolate package)
+    :param small: small pieces of chocolates 1 cm each
+    :param big: Big pieces of chocolates 5 cm each
+    :param x: chocolate package size
+    :return: True if the chocolate pieces can be place in chocolade package
+    """
     small_parts = small * 1
     big_parts = big * 5
     if (small_parts + big_parts <= x):
