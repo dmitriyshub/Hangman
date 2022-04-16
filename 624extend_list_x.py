4739f31d5d953486d87147100b0651ee7c57d38a
dmitriyshub <d.shtranvasser@gmail.com>
2022-04-16 19:02:16 +0300

    final fix 624extend_list_x.py with implamantaion and final answer

--- 624extend_list_x.py
+++ 624extend_list_x.py
@@ -4,8 +4,10 @@ def extend_list_x(list_x, list_y):
     :param list_y: list
     :return: extended list_x with list_y content in the beggining list_x[list_y,list_x]
     """
-    list_x[:0] = list_y
-    return list_x
+    #list_x[:0] = list_y # first method
+    #return list_x # first method
+    list_y[len(list_y):] = list_x # second method
+    return list_y # second method
 
 def main():
     print(extend_list_x([4,5,6],[1,2,3]))
