a0c27ae40fc869d4b3c651b0ca1cdcf8a9d17d35
dmitriyshub <d.shtranvasser@gmail.com>
2022-04-17 17:15:37 +0300

    adding unit-2 folder and 251 252 exersice solution

---
+++ unit2-variables/251welcome_page.py
@@ -0,0 +1,20 @@
+# the Hangman welcome page
+
+HANGMAN_ASCII_ART = ("""
+Welcome to the game Hangman \n
+  _    _                                         
+ | |  | |                                        
+ | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
+ |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+ | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+ |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                      __/ |                      
+                     |___/  \n
+
+
+ """)
+# number of attempts (randomly)
+MAX_TRIES = 6
+
+print(HANGMAN_ASCII_ART, "\n", MAX_TRIES)
+
---
+++ unit2-variables/252user_input.py
@@ -0,0 +1,3 @@
+#The hangman input from the gamer
+letter_answer = input("Guess a letter: ")
+print("Your answer is: ", letter_answer)
