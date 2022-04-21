3b6a3f16a2f92e5c82b66730e2ef49d0f123d189
dmitriyshub <d.shtranvasser@gmail.com>
2022-04-21 22:38:59 +0300

    adding 821format_string.py solution

---
+++ 821format_string.py
@@ -0,0 +1,8 @@
+data = ("self", "py", 1.543)
+format_string = "Hello"
+
+format_string = 'Hello %s.%s learner, you have only %.1f units left before you master the course!' % (data)
+
+print(format_string)
+
+
